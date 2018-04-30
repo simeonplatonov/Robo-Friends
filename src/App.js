3750a8c0f3ac8e0a9188fee0189cec5ce83c9132
Simeon Platonov <simeon.platonov@gmail.com>
2018-04-30 17:22:26 +0300

    App.css path corrected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import {robots} from "./robots";
-import "./app.css";
+import "./App.css";
 class App extends React.Component{
    constructor(){
     super();
